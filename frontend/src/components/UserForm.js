5cc0277a6ceb39070ba0348f59c2603fecba45ed
Vaibhav Dasari <83279965+vaibhavdasari2507@users.noreply.github.com>
2022-12-05 01:01:49 +0530

    completed 2

--- frontend/src/components/UserForm.js
+++ frontend/src/components/UserForm.js
@@ -5,9 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./UserForm.css";
 import { submitUserform } from "../store/actions/userform-actions";
 import { useDispatch } from "react-redux";
-import { useNavigate,Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 const UserForm = () => {
-  const navigate = useNavigate();
     const [data, setData] = useState({
         gender: "",
         favcuisine: "",
@@ -27,15 +26,16 @@ const UserForm = () => {
         e.preventDefault();
         dispatch(submitUserform(data));
         console.log(data);
-        // navigate("/surveylist")
     };
     return (
         <>
             <img src='/survey2.png' alt='' />
-            <Link style={{position:"relative"}}to="/surveylist">Go to survey list</Link>
+            <Link style={{ position: "relative" }} to='/surveylist'>
+                Go to survey list
+            </Link>
             <div className='form'>
                 <h2 style={{ textAlign: "center" }}>Survey Form</h2>
-                <Form onSubmit={submitHandler} method="POST">
+                <Form onSubmit={submitHandler} method='POST'>
                     <Form.Group className='mb-3 pad' controlId='formBasicEmail'>
                         <Form.Label style={{ fontWeight: "bold" }}>Gender</Form.Label>
                         <Form.Control name='gender' type='text' placeholder='Enter Gender' onChange={onChangeHandler} />
@@ -43,11 +43,21 @@ const UserForm = () => {
 
                     <Form.Group className='mb-3 pad' controlId='formBasicPassword'>
                         <Form.Label style={{ fontWeight: "bold" }}>Cuisine</Form.Label>
-                        <Form.Control name='favcuisine' type='text' placeholder='Enter Cuisine' onChange={onChangeHandler}/>
+                        <Form.Control
+                            name='favcuisine'
+                            type='text'
+                            placeholder='Enter Cuisine'
+                            onChange={onChangeHandler}
+                        />
                     </Form.Group>
                     <Form.Group className='mb-3 pad' controlId='formBasicPassword'>
                         <Form.Label style={{ fontWeight: "bold" }}>Current habits</Form.Label>
-                        <Form.Control name='curhabits' type='text' placeholder='Enter Current Habits' onChange={onChangeHandler}/>
+                        <Form.Control
+                            name='curhabits'
+                            type='text'
+                            placeholder='Enter Current Habits'
+                            onChange={onChangeHandler}
+                        />
                     </Form.Group>
                     <Form.Group className='mb-3 pad' controlId='formBasicPassword'>
                         <Form.Label style={{ fontWeight: "bold" }}>Skip Breakfast</Form.Label>
